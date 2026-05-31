eb0ee1d723b7925ba402b9f8aa6754589173bda1
Fábio Dias <fabiojoaodias@gmail.com>
2026-05-31 17:07:34 +0100

    mais

--- src/components/GoogleConversionPageLoad.tsx
+++ src/components/GoogleConversionPageLoad.tsx
@@ -4,15 +4,12 @@ import { useEffect } from 'react'
 
 export default function GoogleConversionPageLoad() {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const w = window as any;
-      if (typeof w.gtag === 'function') {
-        w.gtag('event', 'conversion', {
-          'send_to': 'AW-18188234054/uGDNCLCKgrMcEMba6eBD',
-          'value': 1.0,
-          'currency': 'EUR'
-        });
-      }
+    if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+      window.gtag('event', 'conversion', {
+        'send_to': 'AW-18188234054/uGDNCLCKgrMcEMba6eBD',
+        'value': 1.0,
+        'currency': 'EUR'
+      });
     }
   }, []);
 
